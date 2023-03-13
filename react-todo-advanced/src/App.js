f6ea20dad8b580917f5e79371a6c4824c8b2bb69
Jason Hensley <jasonhensley1989@gmail.com>
2023-03-13 12:57:51 -0500

    css styling with components

--- react-todo-advanced/src/App.js
+++ react-todo-advanced/src/App.js
@@ -1,12 +1,18 @@
+// import React from 'react';
 import Header from "./components/Header";
 
 function App() {
-  const name = true;
   return (
       <div className="container">
           <Header />
       </div>
   );
 }
+
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello From a calss</h1>
+//   }
+// }
 
 export default App;
