8c7987bc9ac4ee1b6997ec35f2eb998068858d3c
Jason Hensley <jasonhensley1989@gmail.com>
2023-03-13 15:21:49 -0500

    add task component complete, prop drills, passing props

--- react-todo-advanced/src/App.js
+++ react-todo-advanced/src/App.js
@@ -1,6 +1,7 @@
 // import React from 'react';
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 import { useState } from 'react';
 
 function App() {
@@ -25,6 +26,13 @@ function App() {
     }
 ])
 
+// Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1;
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
+
 // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -35,9 +43,11 @@ function App() {
     setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder} : task))
   }
 
+
   return (
       <div className="container">
           <Header />
+          <AddTask onAdd={addTask}/>
           {tasks.length > 0 ?<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
       </div>
   );
